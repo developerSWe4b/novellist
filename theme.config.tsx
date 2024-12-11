08f981aa8d3a785d1d793017a85d3c84a893f981
God? R <pxcvhosting@gmail.com>
2024-12-11 16:21:37 +0000

    initial

---
+++ theme.config.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Novel list</span>,
+ 
+  footer: {
+    text: 'Made by n06ody',
+  },
+}
+
+export default config
